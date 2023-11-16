d7d299193134d3c05d8d4813103a16793dbe014d
yasheyxs <jazmingaidoyxs@gmail.com>
2023-11-15 22:10:41 -0300

    comentario

--- indexStruct.js
+++ indexStruct.js
@@ -4,22 +4,18 @@ const contenedor = document.querySelector('.navbar-container');
 // Define una estructura de array de objetos para las rutas del navbar
 const menuItems = [
     { text: 'Inicio', path: '../html/privatehome.html' },
-    { text: 'Productos', path: '../html/productos.html' },
     { text: 'Hamburguesas', path: '../html/burger.html' },
     { text: 'Empanadas', path: '../html/empanadas.html' },
     { text: 'Bebidas', path: '../html/bebidas.html' },
     { text: 'Contacto', path: '../html/privatehome.html#contacto' },
-    { text: 'Carrito', path: '../html/agregar.html' },
+    { text: 'Carrito', path: '../html/carrito.html' },
     { text: 'Cerrar sesión', path: '../index.html', id: 'logout-button' }
 ];
 
-// Función para llenar dinámicamente el navbar
 function fillNavbar() {
     const navbar = document.querySelector('.navbar');
 
-    // Elimina cualquier contenido existente del navbar
     navbar.innerHTML = '';
-
     menuItems.forEach(item => {
         const listItem = document.createElement('a');
         listItem.href = item.path;
--- script.js
+++ script.js
@@ -1,41 +1,14 @@
+function handleLogin(event) {
+    event.preventDefault();
+    window.location.href = '../html/privatehome.html';
+}
+
+function handleRegistration(event) {
+    event.preventDefault();
+    window.location.href = '../html/privatehome.html';
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.querySelector('.navbar');
-
-    function redirectToLogin() {
-        window.location.href = '../html/loginregister.html';
-    }
-
-    navbar.addEventListener('click', (event) => {
-        if (event.target.id === 'logout-button') {
-            redirectToLogin();
-        }
-    });
-
-    const loginForm = document.getElementById('login-form');
-    const registerForm = document.getElementById('register-form');
-
-    if (loginForm) {
-        loginForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            var loggedIn = true;
-
-            if (loggedIn) {
-                redirectToLogin();
-            }
-        });
-    }
-
-    if (registerForm) {
-        registerForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            var registered = true;
-            if (registered) {
-                redirectToLogin();
-            }
-        });
-    }
-});
-
 function stepper(input, isIncrement) {
     let min = parseInt(input.getAttribute("min"));
     let max = parseInt(input.getAttribute("max"));
@@ -58,11 +31,16 @@ fetch('../productos.json')
             const card = document.createElement('div');
             card.classList.add('box');
             card.innerHTML = `
+                <span class="price"> $${product.price}  </span>
                 <img src="${product.image}" alt="${product.name}">
-                <div class="price">${product.price} </div>
                 <h3>${product.name}</h3>
                 <p>${product.description}</p>
-                <button onclick="addToCart(${product.id})">Añadir al carrito</button>
+                <a href="#" class="btn">Pídela ahora</a>
+                <div class="container">
+                    <button class="decrement" onclick="stepper(document.getElementById('my-input-${product.id}'), false)"> - </button>
+                    <input type="number" value="0" min="0" max="20" step="1" id="my-input-${product.id}" readonly>
+                    <button class="increment" onclick="stepper(document.getElementById('my-input-${product.id}'), true)"> + </button>
+                </div>
             `;
             productContainerHambur.appendChild(card);
         });
@@ -120,10 +98,67 @@ fetch('../productos.json')
         });
     })
     .catch(error => console.error('Error fetching products:', error));
+});
 
+/*     // Código del carrito
+let carrito = [];
+function addToCart(productId, quantity) {
+    const product = getProductById(productId);
+    if (product) {
+        const item = {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: quantity || 1,
+        };
+        const existingItemIndex = carrito.findIndex((item) => item.id === productId);
 
-
-
-function addToCart(productId) {
-    // Lógica para agregar productos al carrito
+        if (existingItemIndex !== -1) {
+            carrito[existingItemIndex].quantity += quantity || 1;
+        } else {
+            carrito.push(item);
+        }
+        updateCartView();
+    }
 }
+
+function removeFromCart(productId) {
+    carrito = carrito.filter((item) => item.id !== productId);
+    updateCartView();
+}
+
+function getProductById(productId) {
+    let product = null;
+
+    data.categories.forEach((category) => {
+        const foundProduct = category.products.find((prod) => prod.id === productId);
+        if (foundProduct) {
+            product = foundProduct;
+        }
+    });
+
+    return product;
+}
+
+function updateCartView() {
+    const carritoContainer = document.getElementById('carrito-container');
+    carritoContainer.innerHTML = '';
+
+    carrito.forEach((item) => {
+        const cartItem = document.createElement('div');
+        cartItem.classList.add('cart-item');
+        cartItem.innerHTML = `
+            <div>${item.name}</div>
+            <div>${item.quantity} x $${item.price}</div>
+            <div>Total: $${item.quantity * item.price}</div>
+            <button onclick="removeFromCart(${item.id})">Eliminar</button>
+        `;
+        carritoContainer.appendChild(cartItem);
+    });
+}
+
+function realizarCompra() {
+    console.log('Compra realizada');
+    carrito = [];
+    updateCartView();
+} */
